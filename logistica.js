222e49382f5a4adecda7a5b022b97740175048bc
marianogallo <mag.dis.ind@gmail.com>
2022-03-21 22:06:11 -0300

    correccion

--- logistica.js
+++ logistica.js
@@ -13,28 +13,97 @@
 -cargas pesadas= $1500,
 */
 
-//let cliente = prompt("Ingrese su nombre y apellido");
-let cliente = document.getElementById("usuario")
-//let cuit = prompt("Ingrese su CUIT. Son 11 números sin - ni espacios.");
-let cuit = document.getElementById("usuarioCuit");
+programarBtnCalcular();
+programarBtnLimpiar();
+programarBtnCargar();
 
-while (isNaN(cuit) || cuit.length !== 11) {
-    alert ("El número ingresado es inválido");
-    cuit = prompt("Vuelva a ingresar un número");
+
+
+let inputNombre = document.getElementById("inputUsuarioNombre");
+//inputNombre.addEventListener("change", ()=>{console.log(inputNombre.value)});
+//let inputCuit = document.getElementById("inputUsuarioCuit");
+//inputCuit.addEventListener("change", ()=>{console.log(inputCuit.value)});
+
+
+function programarBtnCalcular() {
+    let formularioValido = false;
+    const btnCalcular = document.querySelector("#btn-calcular");
+    btnCalcular.addEventListener("click", ()=>{
+        console.log(inputNombre.value)
+        formularioValido = validarCuit();
+        if (formularioValido)
+        calcularDistancia();
+        elegirServicio();
+    })
 }
-alert("Su CUIT fué registrado correctamente");
 
-console.log("El cliente " + cliente + " se registró con el CUIT: " + cuit);
-
-const puntoA= parseInt(prompt("Ingrese punto de partida"));
-const puntoB= parseInt(prompt("Ingrese punto de llegada"));
-
-function calcularDistancia(a, b){
-    distancia= puntoB - puntoA;
+function calcularDistancia(){
+    const inputPuntoA = document.querySelector("#puntoPartida")
+        //console.log(inputPuntoA.value)
+    const inputPuntoB = document.querySelector("#puntoLlegada")
+        //console.log(inputPuntoB.value)
+    const distancia = inputPuntoB.value - inputPuntoA.value;
+    
+    console.log("La distancia del recorrido es: " + distancia + "km");
     return distancia;
 }
-console.log("La distancia es: " + calcularDistancia() + "km");
 
+
+function validarCuit(){
+    const inputCuit = document.querySelector("#inputUsuarioCuit")
+    
+        if(inputCuit.value.length !== 11){
+        
+            alert("vuelva a ingresar un numero") 
+            inputCuit.focus();
+            return false;
+        }
+        console.log("CUIT " + inputCuit.value + " se registró correctamente");
+        return true;
+}
+
+function elegirServicio(){
+    
+    let opcionCadeteria = document.querySelector('#cadeteria');
+    let opcionFlete = document.querySelector('#flete');
+    let opcionCargasPesadas = document.querySelector('#cargasPesadas');
+    let opcionMudanza = document.querySelector('#mudanza');
+    
+    const servicio = document.querySelector("#tipoServicio");
+    servicio.addEventListener("change", ()=>{
+        if (opcionCadeteria.checked) {
+            console.log("La opcion seleccionada es: " + opcionCadeteria.value);
+        } 
+        else if(opcionFlete.checked){
+            console.log("La opcion seleccionada es: " + opcionFlete.value)
+        }
+        else if(opcionCargasPesadas.checked){
+            console.log("La opcion seleccionada es: " + opcionCargasPesadas.value)
+        }
+        else if(opcionMudanza.checked){
+            console.log("La opcion seleccionada es: " + opcionMudanza.value)
+        }
+    })
+}
+
+//--------------------------------------------------------------------------------------
+function programarBtnLimpiar() {
+    const btnLimpiar = document.querySelector("#btn-limpiar")
+    btnLimpiar.onclick = ()=>{
+    
+    }
+}
+//--------------------------------------------------------------------------------------
+function programarBtnCargar() {
+    const btnCargar = document.querySelector("#btn-cargar")
+    btnCargar.onclick = ()=>{
+    alert("PROGRAMA BTN CARGAR");
+    }
+}
+
+
+
+/*
 const servicio = prompt("Ingrese el tipo de servivcio: cadeteria | flete | cargasPesadas| mudanza");
 switch (servicio) {
     case "cadeteria":{
@@ -60,8 +129,7 @@ switch (servicio) {
         break;
 }
 
-
-//Desafío: Incorporar Arrays
+Desafío: Incorporar Arrays
 const vehiculos = ["motocicleta", "utilitario chico", "utilitario grande", "camion"];
 for (let i = 0; i < vehiculos.length; i++) {
 //    console.log(vehiculos[i]);
@@ -83,66 +151,12 @@ for (let i = 0; i < vehiculos.length; i++) {
         console.log("Vehiculo no encontrado");
     }
 } 
-/*
-const choferes = [
-    {legajo: 0001,
-    apellido: "Gomez",
-    nombre: "Pedro",
-    licencia: "A3"
-    },
-    {legajo: 0002,
-    apellido: "Perez",
-    nombre: "Juan",
-    licencia: "A3"
-    },
-    {legajo: 0003,
-    apellido: "Garcia",
-    nombre: "Carlos",
-    licencia: "A3"
-    },
-    {legajo: 0004,
-    apellido: "Martinez",
-    nombre: "Juan",
-    licencia: "B2"
-    },
-    {legajo: 0005,
-    apellido: "Lopez",
-    nombre: "Tomas",
-    licencia: "B2"
-    },
-    {legajo: 0006,
-    apellido: "Silva",
-    nombre: "Pablo",
-    licencia: "C3"
-    }
-];
-
-choferes.push({legajo: 0007, apellido: "Juarez", nombre: "Juan Pablo", licencia: "C3"});
-
-for(const chofer of choferes){
-    console.log("chofer: " + chofer.apellido + " " + chofer.nombre);
-    console.log("licencia: " + chofer.licencia);
 */
-
 
 //9- Crear una class para definir choferes y agregarlos a un array que liste cada uno de los empleados.-(chofer.js)
 //10- Para el vehículo motocicleta mostrar los nombres de los choferes con licencia A3.-
 //    Para los vehículos utilitarios mostrar los nombres de los choferes con licencia B2.-
 //    Para el vehículo camion mostrar los nombres de los choferes con licencia C3.-
 
-//DESAFÍO COMPLEMENTARIO - Interactuar con HTML
-//crear las opciones con radoi buttnon para e tipo de viaje
-
-const servicios = document.getElementById("tipoServicio")
-servicios.innerHTML = `
-<h3>Seleccione el servicio</h3> 
-    <input type="radio" id="cadeteria" name="cadeteria" value="cadeteria" checked>CADETERIA</input>
-    <input type="radio" id="flete" name="flete" value="flete" checked>FLETE</input>
-    <input type="radio" id="cargasPesadas" name="cargasPesadas" value="cargasPesadas" checked>CARGAS PESADAS</input>
-    <input type="radio" id="mudanza" name="mudanza" value="mudanza" checked>MUDANZA</input>
-` ;
-
-seleccionServicio = document.createElement
-                                                
-
-
+//const servicios = document.getElementById("tipoServicio")
+//servicios.innerHTML = `<label><span>Seleccione el servicio</span></label>` 
---
+++ vehiculo.js
@@ -0,0 +1,31 @@
+class Vehiculo{
+    constructor(movil, tipo, marca, modelo, ano, patente){
+        this.movil = movil;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.patente = patente;
+    }
+}
+
+let vehiculos = [];
+vehiculos.push(new Vehiculo(0011, "motocicleta", "Honda", "GLH 150", 2017, "A097WER"));
+vehiculos.push(new Vehiculo(0027, "motocicleta", "Honda", "GLH 150", 2017, "A098PLU"));
+vehiculos.push(new Vehiculo(0043, "motocicleta", "Bajaj", "BOXER 150", 2019, "A099YTR"));
+vehiculos.push(new Vehiculo(0014, "utilitario", "Renault", "Kangoo", 2018, "AB123CD"));
+vehiculos.push(new Vehiculo(0025, "utilitario", "Peugeot", "Berlingo", 2017, "AB321DC"));
+vehiculos.push(new Vehiculo(0006, "utilitario", "Fiat", "Fiorino", 2018, "AC453CD"));
+vehiculos.push(new Vehiculo(0057, "furgon", "Renault", "Master", 2020, "AD143CE"));
+vehiculos.push(new Vehiculo(0010, "furgon", "Mercedes Benz", "Sprint", 2019, "AD089CE"));
+vehiculos.push(new Vehiculo(0017, "furgon", "Mercedes Benz", "Sprint", 2019, "AD295TE"));
+vehiculos.push(new Vehiculo(0012, "camion", "Iveco", "Tector 90-190", 2015, "AD045CG"));
+vehiculos.push(new Vehiculo(0034, "utilitario", "Fiat", "Fiorino", 2018, "AE768RD"));
+vehiculos.push(new Vehiculo(0045, "utilitario", "Fiat", "Fiorino", 2018, "AY553KD"));
+
+//console.log(vehiculos.tipo);
+/*
+for(const vehiculo of vehiculos){
+    console.log(vehiculo.tipo + " " + vehiculo.marca + " " + vehiculo.modelo)
+}
+*/
